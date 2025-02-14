85cabfde6b6b1b844a55f610f7dc9449c90dfff9
maksemen2 <lxshnaps@gmail.com>
2025-02-14 23:55:33 +0300

    refactor auth middleware to use structured error responses

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -6,31 +6,32 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/maksemen2/avito-task/internal/auth"
+	"github.com/maksemen2/avito-task/internal/models"
 )
 
 func AuthMiddleware(jwtManager *auth.JWTManager) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		tokenStr := c.GetHeader("Authorization")
 		if tokenStr == "" {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			c.AbortWithStatusJSON(http.StatusUnauthorized, models.ErrorResponse{Errors: models.ErrUnauthorized})
 			return
 		}
 
 		t, err := jwtManager.ParseToken(tokenStr)
 		if err != nil || !t.Valid {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			c.AbortWithStatusJSON(http.StatusUnauthorized, models.ErrorResponse{Errors: models.ErrUnauthorized})
 			return
 		}
 
 		claims, ok := t.Claims.(jwt.MapClaims)
 		if !ok {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			c.AbortWithStatusJSON(http.StatusUnauthorized, models.ErrorResponse{Errors: models.ErrUnauthorized})
 			return
 		}
 
 		userIDFloat, ok := claims["userID"].(float64)
 		if !ok {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			c.AbortWithStatusJSON(http.StatusUnauthorized, models.ErrorResponse{Errors: models.ErrUnauthorized})
 			return
 		}
 		c.Set("UserID", uint(userIDFloat))
